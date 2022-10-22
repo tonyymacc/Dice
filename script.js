975f534a912472ea69a7986b8ede0dbf9d499873
Tony Leonardo <tonyleo.mac@gmail.com>
2022-10-22 02:41:56 -0800

    Add factory function for player creation

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function randomNumberGenerator(max) {
-    return max = Math.floor(Math.random() * max) + 1;
- };
- 
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.dieOne = randomNumberGenerator(6);
-        this.dieTwo = randomNumberGenerator(6);
-        this.dieTotal = this.dieOne + this.dieTwo;
-        this.winMessageOne = name + " wins!";
-        this.winMessageTwo = name + " wins with a DOUBLE!";
-    };
+const Player = (name) => {
+    const RNG = (max) => max = Math.floor(Math.random() * 6) + 1;
+    const dieOne = RNG();
+    const dieTwo = RNG();
+    const dieTotal = () => dieOne + dieTwo    
+    const winMessageOne = () => console.log(name, "wins!");
+    const winMessageTwo = () => console.log(name, "wins with a DOUBLE!");
+    return {name, dieOne, dieTwo, dieTotal, winMessageOne, winMessageTwo}
 };
 
 const tie = {
@@ -18,30 +13,28 @@ const tie = {
      messageTwo: "It's a tie with a DOUBLE!",
  };
 
-const player1= new Player("Player");
-const player2 = new Player("Computer");
+const player1 = Player("Player");
+const player2 = Player("Computer");
  
- function gameStart() {
-
+ function gameStart() {    
     console.log("\n", "🎲",player1.dieOne, "🎲", player1.dieTwo, "\n", "🎲", player2.dieOne, "🎲", player2.dieTwo);
     if (player1.dieOne === player1.dieTwo && player2.dieOne === player2.dieTwo ||
         player1.dieOne === player2.dieOne && player1.dieTwo === player2.dieTwo) {
         console.log(tie.messageTwo);
     }
     else if (player1.dieOne === player1.dieTwo && player2.dieOne !== player2.dieTwo) {
-        console.log(player1.winMessageTwo);
+        player1.winMessageTwo();
     }
     else if (player2.dieOne === player2.dieTwo && player1.dieOne !== player1.dieTwo) {
-        console.log(player2.winMessageTwo);
+        player2.winMessageTwo();
     }
-    else if (player1.dieTotal === player2.dieTotal) {
+    else if (player1.dieTotal() === player2.dieTotal()) {
         console.log(tie.messageOne);
     }
-    else if (player1.dieTotal > player2.dieTotal) {
-        console.log(player1.winMessageOne);
+    else if (player1.dieTotal() > player2.dieTotal()) {
+        player1.winMessageOne();
     }
     else {
-        console.log(player2.winMessageOne);
+        player2.winMessageOne();
     };
  };
- gameStart();
